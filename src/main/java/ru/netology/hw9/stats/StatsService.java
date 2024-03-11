a8afe68d0ef253844695f86e8ed3cf55449c098f
Danila Leonidov <3317451@gmail.com>
2024-03-12 00:01:08 +0300

    change

--- src/main/java/ru/netology/hw9/stats/StatsService.java
+++ src/main/java/ru/netology/hw9/stats/StatsService.java
@@ -1,64 +1,64 @@
 package ru.netology.hw9.stats;
 
 public class StatsService {
-        public long sum(long[] sales) {
-            long totalSale = 0;
-            for (long sale : sales) {
-                totalSale += sale;
-            }
-            return totalSale;
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
         }
-
-        public long average(long[] sales) {
-            long totalSale = 0;
-            for (long sale : sales) {
-                totalSale += sale;
-            }
-            return totalSale / sales.length;
-        }
-
-        public int monthMaximumSale(long[] sales) {
-            int monthMaximum = 0;
-
-            for (int i = 0; i < sales.length; i++) {
-                if (sales[i] >= sales[monthMaximum]) {
-                    monthMaximum = i;
-                }
-            }
-            return monthMaximum + 1;
-        }
-
-        public int monthMinimumSale(long[] sales) {
-            int monthMinimum = 0;
-
-            for (int i = 0; i < sales.length; i++) {
-                if (sales[i] <= sales[monthMinimum]) {
-                    monthMinimum = i;
-                }
-            }
-            return monthMinimum + 1;
-        }
-
-        public int salesBellowAverage(long[] sales) {
-            long averageSales = average(sales);
-            int counter = 0;
-            for (long sale : sales) {
-                if (sale < averageSales) {
-                    counter++;
-                }
-            }
-            return counter;
-        }
-
-        public int salesOverAverage(long[] sales) {
-            long averageSales = average(sales);
-            int counter = 0;
-            for (long sale : sales) {
-                if (sale > averageSales) {
-                    counter++;
-                }
-            }
-            return counter;
-        }
+        return totalSale;
     }
+
+    public long average(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale / sales.length;
+    }
+
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaximum]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int monthMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMinimum]) {
+                monthMinimum = i;
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+    public int salesBellowAverage(long[] sales) {
+        long averageSales = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < averageSales) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int salesOverAverage(long[] sales) {
+        long averageSales = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > averageSales) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
 
